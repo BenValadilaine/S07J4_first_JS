fff5d8ab0bb917810f1e04f19a34994c38aaa659
BenValadilaine <benjamin.valadilaine@gmail.com>
2020-08-19 21:21:11 +0200

    processing 2.4.1

--- script_6.js
+++ script_6.js
@@ -1,8 +1,22 @@
-const adn = [
-  CCGUCGUUGCGCUACAGC,
-  CCUCGCCGGUACUUCUCG
-]
+let adn1 = "CCGUCGUUGCGCUACAGC";
+let adn2 = "CCUCGCCGGUACUUCUCG";
+let aminoAcid = {
+  serine: ["UCU", "UCC", "UCA", "UCG", "AGU", "AGC"],
+  proline: ["CCU", "CCC", "CCA", "CCG"],
+  leucine: ["UUA", "UUG"],
+  phenylalaline: ["UUU", "UUC"],
+  arginine: ["CGU", "CGC", "CGA", "AGA", "AGG"],
+  tyrosine: ["UAU", "UAC"],
+};
 
-adn.forEach(content => {
+//Let's cut them!
+let adn1cut = adn1.match(/.{1,3}/g);
+console.log("adn1 : " + adn1cut)
+let adn2cut = adn2.match(/.{1,3}/g);
+console.log("adn2 : " + adn2cut)
 
+aminoAcid.forEach(amino => {
+  if (amino.includes(adn1)){
+// vérifier s'il y a une séquence d'acide aminé contenu dans l'adn1
+  }
 });
